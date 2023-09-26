f52d7d544b549d8b690e4ff14f847d18a9d651d4
Gabriel Romero <gabriel.romero@utec.edu.pe>
2023-09-25 21:23:38 -0500

    avances

--- code/R_Logistica.py
+++ code/R_Logistica.py
@@ -1,55 +1,71 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
-from PIL import Image
-import random
 
 class RegresionLogisticaMultinomial:
     def __init__(self, x, y, alpha=0.01, epochs=1000):
         self.x = x
-        self.y = y
+        self.y = y - np.min(y)
         self.alpha = alpha
         self.epochs = epochs
-        self.num_classes = len(np.unique(y))  # Número de clases
-        self.num_features = x.shape[1]        # Número de características
+        self.num_classes = len(np.unique(y))
+        self.num_features = x.shape[1]
         self.weights = np.zeros((self.num_features, self.num_classes))
+        self.losses = []
 
     def hiperplano(self):
         return np.dot(self.x, self.weights)
 
-    def softmax(self):
-        logits = self.hiperplano()
+    def softmax(self, logits):
         exp_logits = np.exp(logits - np.max(logits, axis=1, keepdims=True))
         probabilities = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
         return probabilities
 
-    def loss_function(self):
-        probabilities = self.softmax()
-        n = self.x.shape[0]
-        loss = -np.sum(np.log(probabilities[range(n), self.y])) / n
+    def loss_function(self, logits, y_true):
+        probabilities = self.softmax(logits)
+        n = len(y_true)
+        epsilon = 1e-15
+        loss = -np.sum(np.log(probabilities[range(n), y_true] + epsilon)) / n
         return loss
 
-    def gradient(self):
-        probabilities = self.softmax()
-        n = self.x.shape[0]
-        gradient = np.dot(self.x.T, (probabilities - np.eye(self.num_classes)[self.y])) / n
+    def gradient(self, logits, y_true):
+        probabilities = self.softmax(logits)
+        n = len(y_true)
+        gradient = np.dot(self.x.T, (probabilities - np.eye(self.num_classes)[y_true])) / n
         return gradient
     
     def change_parameters(self, gradient, w):
         return w - self.alpha * gradient
     
-    def train(self):
+    def train(self, x_val=None, y_val=None):
         n = len(self.x)
+        val_losses = []  # Agregar una lista para almacenar las pérdidas de validación
+
         for _ in range(self.epochs):
             sample_idx = np.random.choice(n, n, replace=True)
-            x_train, y_sample = self.x[sample_idx], self.y[sample_idx]
-            gradient = self.gradient()
+            x_sample, y_sample = self.x[sample_idx], self.y[sample_idx]
+            logits = np.dot(x_sample, self.weights)
+            self.losses.append(self.loss_function(logits, y_sample))
+            gradient = self.gradient(logits, y_sample)
             self.weights = self.change_parameters(gradient, self.weights)
 
+            # Si se proporcionan datos de validación, calcula la pérdida de validación
+            if x_val is not None and y_val is not None:
+                val_logits = np.dot(x_val, self.weights)
+                val_loss = self.loss_function(val_logits, y_val)
+                val_losses.append(val_loss)
+
+        return val_losses  # Devuelve las pérdidas de validación
+
     def predict(self, x_test):
-        probabilities = np.dot(x_test, self.weights)
-        predicted_classes = np.argmax(probabilities, axis=1)
+        logits = np.dot(x_test, self.weights)
+        predicted_classes = np.argmax(logits, axis=1)
         return predicted_classes
 
+    def plot_loss(self):
+        plt.plot(self.losses)
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('Loss vs. Epochs')
+        plt.show()
 
--- code/main.py
+++ code/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import carga_data, Kdtree, R_Logistica, Kdtree, svm, arbol
 import matplotlib.pyplot as plt
 
-def metrics_summary(y_true, y_pred):
+def metrics_summary(y_true, y_pred, verbose=True):
     classes = np.unique(y_true)
     confusion = np.zeros((len(classes), len(classes)))
     for i, class_true in enumerate(classes):
@@ -24,7 +24,18 @@ def metrics_summary(y_true, y_pred):
         'Recall': recall,
         'F1-Score': f1_score
     })
-    print(results_df)
+    
+    # Contador de clases con precisión positiva
+    count_positive_precision = sum(precision > 0)
+    
+    # Calcula la precisión promedio
+    avg_precision = np.mean(precision)
+    
+    if verbose:
+        print(results_df)
+    
+    return count_positive_precision, avg_precision, f1_score
+    
 
 def svm_ex():
     directorio_especies = r"C:\Users\lords\Desktop\ML-Proyecto1\code\images"
@@ -70,51 +81,47 @@ def arbol_ex():
 
     metrics_summary(y_test, y_pred)
 
+def rlogistica_exc(seed=None):
+    directorio_especies = r"C:\Users\lords\Desktop\ML-Proyecto1\code\images"
+    nueva_res = (128, 128)
+    
+    np.random.seed(seed)  # Establece la semilla
+    
+    carga_data.crear_carpetas(directorio_especies)
+    carga_data.organizar_imagenes(directorio_especies, nueva_res, directorio_especies)
+    imagenes, etiquetas = carga_data.leer_imagenes(directorio_especies)
+    
+    cortes = 3
+    imagenes_vectorizadas = carga_data.vectorizar_imagenes(imagenes, cortes)
+    data = carga_data.crear_dataframes(imagenes_vectorizadas, etiquetas)
+    
+    train_data, val_data, test_data = carga_data.dividir_datos(data)
+    
+    model = R_Logistica.RegresionLogisticaMultinomial(train_data.drop(columns=['Etiqueta']).to_numpy(), train_data['Etiqueta'].to_numpy(), alpha=0.01, epochs=1000)
+    val_losses = model.train(x_val=val_data.drop(columns=['Etiqueta']).to_numpy(), y_val=val_data['Etiqueta'].to_numpy())    
+    y_pred = model.predict(test_data.drop(columns=['Etiqueta']).to_numpy())
+    
+    # Aquí, calculamos el F1-Score promedio y lo devolvemos para usarlo como métrica de referencia
+    count_positive_precision, avg_precision, f1_scores = metrics_summary(test_data['Etiqueta'].to_numpy(), y_pred, verbose=True)
+
+    plt.plot(val_losses, label='Validation Loss')
+    plt.legend()
+    plt.show()
+
+def Kdtree_exc():
+    directorio_especies = r"C:\Users\lords\Desktop\ML-Proyecto1\code\images"
+    nueva_res = (128, 128)
+    carga_data.crear_carpetas(directorio_especies)
+    carga_data.organizar_imagenes(directorio_especies, nueva_res, directorio_especies)
+    imagenes, etiquetas = carga_data.leer_imagenes(directorio_especies)  # Lee las imágenes y sus etiquetas
+    cortes = 3  # Define la cantidad de cortes que se realizarán en cada imagen para vectorizarla
+    imagenes_vectorizadas = carga_data.vectorizar_imagenes(imagenes, cortes) # Vectoriza las imágenes
+    data = carga_data.crear_dataframes(imagenes_vectorizadas, etiquetas)  # Crea los dataframes para entrenamiento y prueba
+    train_data, val_data, test_data = carga_data.dividir_datos(data)  # Divide los datos en conjuntos de entrenamiento y prueba
+
+    tree = Kdtree.Kdtree(imagenes_vectorizadas, etiquetas)
+
+    tree.searchknn(imagenes_vectorizadas[21], 10)
+    
 if __name__ == "__main__":
-    arbol_ex()
-
-
-    #Creacion del Kdtree
-    # Knn = Kdtree.Kdtree(imagenes_vectorizadas, etiquetas)  # Crea el Kdtree con las imágenes y sus etiquetas correspondientes
- 
-    # #buscando los knn mas similares
-    # Knn.searchknn(imagenes[20], 10)  # Busca los 10 vecinos más cercanos a la imagen en la posición 20 del conjunto de imágenes
-
-    # X_train = train_data.drop(columns=['Etiqueta']).to_numpy()
-    # y_train = val_data['Etiqueta'].to_numpy()
-
-    # Rlogistica = R_Logistica.RegresionLogisticaMultinomial(X_train, y_train)
-
-    # Rlogistica.train()
-
-    # # Predecimos el conjunto de prueba
-    # X_test = test_data.drop(columns=['Etiqueta']).to_numpy()
-    # y_test = test_data['Etiqueta'].to_numpy()
-
-    # y_pred = Rlogistica.predict(X_test)
-
-    # # Calculamos la precisión
-    # accuracy = np.sum(y_pred == y_test)
-
-    # print(f"La precisión es: {accuracy}, de un total de {len(y_test)} imágenes de prueba")
-
-#  # Datos para el árbol de decisión
-#     X_train = train_data.drop(columns=['Etiqueta']).to_numpy()
-#     y_train = train_data['Etiqueta'].to_numpy()
-
-#     # Entrenando el árbol de decisión
-#     tree = arbol.DT()
-#     tree.train(X_train, y_train)
-
-#     # Predecimos el conjunto de prueba
-#     X_test = test_data.drop(columns=['Etiqueta']).to_numpy()
-#     y_test = test_data['Etiqueta'].to_numpy()
-
-#     y_pred = tree.predict(X_test)
-
-#     # Calculamos la precisión
-#     accuracy = np.sum(y_pred == y_test)
-
-#     print(f"La precisión del árbol de decisión es: {accuracy / len(y_test) * 100:.2f}% de un total de {len(y_test)} imágenes de prueba")
-    
-    
+    rlogistica_exc()
